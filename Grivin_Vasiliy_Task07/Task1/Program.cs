ad5201791d53a76c5c2c359e6fd2a6596dffbeb5
VasDar <48258166+VasDar@users.noreply.github.com>
2019-04-28 23:20:32 +0300

    Add files via upload

---
+++ Grivin_Vasiliy_Task07/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    interface IIndexable<T>
+    {
+        T this[int index]
+        {
+            get;
+            set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = new int[2];
+
+            myArr[0] = 5;
+            myArr[1] = 8;
+
+            DynamicArray<int> dynamicArray = new DynamicArray<int>(myArr);
+
+            int[] myArray = new int[8];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i;
+            }
+
+            Console.WriteLine(dynamicArray);
+
+            dynamicArray.Insert(2, 3);
+
+            //dynamicArray.Add(2);
+
+            //dynamicArray.Remove(5);
+
+            //dynamicArray.AddRange(myArray);
+
+            Console.WriteLine(dynamicArray);
+            Console.WriteLine(dynamicArray[2]);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
